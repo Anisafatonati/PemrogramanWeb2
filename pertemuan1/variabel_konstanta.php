118ded2d664109a4fd842f8e5b11298e978bb9b3
Anisafatonati <114457553+Anisafatonati@users.noreply.github.com>
2023-02-26 23:19:01 +0700

    Add files via upload

---
+++ pertemuan1/variabel_konstanta.php
@@ -0,0 +1,9 @@
+<?php
+//Cara pertama
+const PRODI = "Teknik Informatika";
+
+//Cara Kedua
+define("ROMBEL", "TI06");
+
+echo "Prodi saya : " . PRODI . "<br>";
+echo "Rombel saya : " . ROMBEL;
---
+++ pertemuan1/variabel_user.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Anisa Fatonati";
+$kelas = "TI06";
+$ipk = 3.98;
+
+echo "Nama saya adalah " . $nama . "<br>";
+echo "Kelas $kelas <br>";
+echo "Ipk saya adalah " . $ipk;
